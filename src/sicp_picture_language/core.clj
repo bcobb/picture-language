af43aa43bab9b8c29633093408f35d5005d99c5e
Brian Cobb <bcobb@uwalumni.com>
2014-12-03 20:24:04 +0000

    Straight SICP-style implementation of vectors and frames

--- src/sicp_picture_language/core.clj
+++ src/sicp_picture_language/core.clj
@@ -1,6 +1,67 @@
 (ns sicp-picture-language.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-vect
+  "Create a two-dimensional vector"
+  [x y]
+  [x y])
+
+(defn xcor-vect
+  "Return the x-coordinate of the given vector"
+  [v]
+  (first v))
+
+(defn ycor-vect
+  "Return the y-coordinate of the given vector"
+  [v]
+  (second v))
+
+(defn add-vect
+  "The result of adding two vectors together"
+  [v w]
+  (make-vect (+ (xcor-vect v) (xcor-vect w))
+             (+ (ycor-vect v) (ycor-vect w))))
+
+(defn sub-vect
+  "The result of subtracting the second vector from the first"
+  [v w]
+  (make-vect (- (xcor-vect v) (xcor-vect w))
+             (- (ycor-vect v) (ycor-vect w))))
+
+(defn scale-vect
+  "The result of scaling the given vector by the given constant"
+  [c v]
+  (make-vect (* (xcor-vect v) c)
+             (* (ycor-vect v) c)))
+
+(defn make-frame
+  "Create a frame at the given origin with the given edges"
+  [origin edge1 edge2]
+  [origin edge1 edge2])
+
+(defn- third [v] (nth v 2))
+
+(defn origin-frame
+  "Return the origin of the given frame"
+  [frame]
+  (first frame))
+
+(defn edge1-frame
+  "Return the first edge of the given frame"
+  [frame]
+  (second frame))
+
+(defn edge2-frame
+  "Return the second edge of the given frame"
+  [frame]
+  (third frame))
+
+(defn frame-coord-map
+  "Returns a procedure that maps a vector to the corresponding vector within
+  the frame"
+  [frame]
+  (fn [v]
+    (add-vect (origin-frame frame)
+              (add-vect (scale-vect (xcor-vect v)
+                                    (edge1-frame frame))
+                        (scale-vect (ycor-vect v)
+                                    (edge2-frame frame))))))
--- test/sicp_picture_language/core_test.clj
+++ test/sicp_picture_language/core_test.clj
@@ -2,6 +2,53 @@
   (:require [clojure.test :refer :all]
             [sicp-picture-language.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-xcor
+  (testing "We can retrieve a vector's x coordinate"
+    (is (= (xcor-vect (make-vect 1 2)) 1))))
+
+(deftest test-ycor
+  (testing "We can retrieve a vector's y coordinate"
+    (is (= (ycor-vect (make-vect 1 2)) 2))))
+
+(deftest test-vector-addition
+  (testing "Adding two vectors together"
+    (is (= (add-vect (make-vect 1 2) (make-vect 2 3)) (make-vect 3 5)))))
+
+(deftest test-vector-subtraction
+  (testing "Subtracting one vector from another"
+    (is (= (sub-vect (make-vect 1 2) (make-vect 2 3)) (make-vect -1 -1)))))
+
+(deftest test-vector-scaling
+  (testing "Scaling a vector by a constant"
+    (is (= (scale-vect 10 (make-vect 1 2)) (make-vect 10 20)))))
+
+(deftest test-frame-origin
+  (testing "We can retrieve a frame's origin"
+    (let [origin (make-vect 0 0)
+          edge1 (make-vect 0 1)
+          edge2 (make-vect 1 0)]
+      (is (= (origin-frame (make-frame origin edge1 edge2)) (make-vect 0 0))))))
+
+(deftest test-frame-edge1
+  (testing "We can retrieve a frame's first edge"
+    (let [origin (make-vect 0 0)
+          edge1 (make-vect 0 1)
+          edge2 (make-vect 1 0)]
+      (is (= (edge1-frame (make-frame origin edge1 edge2)) (make-vect 0 1))))))
+
+(deftest test-frame-edge2
+  (testing "We can retrieve a frame's second edge"
+    (let [origin (make-vect 0 0)
+          edge1 (make-vect 0 1)
+          edge2 (make-vect 1 0)]
+      (is (= (edge2-frame (make-frame origin edge1 edge2)) (make-vect 1 0))))))
+
+(deftest test-frame-coordmap
+  (let [origin (make-vect 5 5)
+        edge1 (make-vect 0 2)
+        edge2 (make-vect 2 0)
+        frame (make-frame origin edge1 edge2)]
+    (testing "0,0 maps to the frame origin"
+      (is (= ((frame-coord-map frame) (make-vect 0 0)) (make-vect 5 5))))
+    (testing "1,1 maps to the relative unit diagonal"
+      (is (= ((frame-coord-map frame) (make-vect 1 1)) (make-vect 7 7))))))
